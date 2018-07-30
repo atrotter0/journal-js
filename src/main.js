4e708d9693b0b991383e35161585cf1e9fad011d
Abel Trotter and Kevin Ahn <kevin@>
2018-07-30 10:48:50 -0700

    Add front-end display and styles for results

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import { Entry } from './journal';
 import './styles.css';
 
+function displayResults(vowelCount, consenantCount) {
+  $(".results").show();
+  $("#vowel-count").text(vowelCount);
+  $("#consenant-count").text(consenantCount);
+}
+
 $(document).ready(function() {
   $("#submit").submit(function(event) {
     event.preventDefault();
+
     var entry = new Entry($("#title").val(), $("#body").val());
-    console.log(entry.countVowels());
-    console.log(entry.countConsonants());
+    displayResults(entry.countVowels(), entry.countConsonants());
   });
 });
