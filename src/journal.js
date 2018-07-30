aa88d595d5c23b2607c1e4d913047c4295ab2237
Abel Trotter and Kevin Ahn <kevin@>
2018-07-30 10:28:56 -0700

    Add prototypes to log vowels and consonents

--- src/journal.js
+++ src/journal.js
@@ -1,9 +1,22 @@
 var vowels = [ "a", "e", "i", "o", "u" ];
 
-export function countVowels(text) {
-  var counter = 0;
-  for (var i = 0; i < text.length; i++) {
-    if (vowels.includes(text[i])) counter++;
+export function Entry(title, body) {
+  this.title = title;
+  this.body = body;
+}
+
+Entry.prototype.countVowels = function() {
+  var vowelCounter = 0;
+  for (var i = 0; i < this.body.length; i++) {
+    if (vowels.includes(this.body[i])) vowelCounter++;
   }
-  return counter;
+  return vowelCounter;
+}
+
+Entry.prototype.countConsonants = function() {
+  var consonantCounter = 0;
+  for (var i = 0; i < this.body.length; i++) {
+    if (!(vowels.includes(this.body[i])) && this.body[i] !== " ") consonantCounter++;
+  }
+  return consonantCounter;
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import { countVowels } from './journal';
+import { Entry } from './journal';
 import './styles.css';
 
 $(document).ready(function() {
-  console.log(countVowels("blah blah blah"));
+  $("#submit").submit(function(event) {
+    event.preventDefault();
+    var entry = new Entry($("#title").val(), $("#body").val());
+    console.log(entry.countVowels());
+    console.log(entry.countConsonants());
+  });
 });
